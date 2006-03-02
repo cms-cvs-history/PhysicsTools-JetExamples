13857fb2186be045fb561e09062fbe5977687e7c
cmsbuild <>
2006-03-02 17:24:33 +0000

    This commit was manufactured by cvs2git to create tag 'PhysicsTools- JetExamples-V00-01-03'.
    Sprout from master 2006-03-02 17:24:27 UTC Stefano Argiro <sha1-27144880fec262534c9bc969ae330367d420f8b3@cern.ch> 'fixed include'
    Cherrypick from master 2006-02-23 15:25:24 UTC Luca Lista <sha1-73801ca8ca16918e053d407e1ed9c2469728f170@cern.ch> 'PhysicsTools/JetExamples/TagLog':
        TagLog
    Delete:
        interface/JetTSelector.h
        src/JetTSelector.cc
        src/classes.h
        src/classes_def.xml
        test/jetschain.C

--- test/jetschain.C
+++
@@ -1,9 +0,0 @@
-{
-   gSystem->Load("libPhysicsToolsFWLite");
-   AutoLibraryLoader::enable();
-   TChain jets("Events");
-   jets.Add("jets.root");
-   gSystem->Load( "libPhysicsToolsJetExamples" );
-   JetTSelector * selector = new JetTSelector;
-   jets.Process( selector );
-}
